627d901ed7674d481a7027e0de5d4ef2cbc5efb9
Takieddine Messaoudi <takieddine.messaoudi.official@gmail.com>
2025-08-04 09:54:25 +0200

    refacto & updates & added init to handle proper shutdown signals

--- docker/config/dist/bin/base-bin/global-stack-base-dump-pg-project.sh
+++ docker/config/dist/bin/base-bin/global-stack-base-dump-pg-project.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-PGPASSWORD=$DATABASE_PASSWORD pg_dump --no-owner -h $DATABASE_HOST -U $DATABASE_USER -w -Fc $DATABASE_NAME > "${1}"
+PGPASSWORD=$DATABASE_PASSWORD pg_dump --no-owner -h $DATABASE_HOST -p ${DATABASE_PORT:-5432} -U $DATABASE_USER -w -Fc $DATABASE_NAME > "${1}"
+echo "Finished"
--- docker/config/dist/bin/base-bin/global-stack-base-import-pg-project-dump-if-database-empty.sh
+++ docker/config/dist/bin/base-bin/global-stack-base-import-pg-project-dump-if-database-empty.sh
@@ -6,10 +6,12 @@ else
     echo "Importing database dump ${1} to database: ${DATABASE_NAME}, server: ${DATABASE_HOST}"
     if [[ "${1}" == *\.bak ]]; then
         echo "pg_restore .bak"
-        PGPASSWORD=${DATABASE_PASSWORD} pg_restore --no-owner -h ${DATABASE_HOST} -U ${DATABASE_USER} -d ${DATABASE_NAME} -Fc "${1}"
+        PGPASSWORD=${DATABASE_PASSWORD} pg_restore --no-owner -h ${DATABASE_HOST} -p ${DATABASE_PORT:-5432} -U ${DATABASE_USER} -d ${DATABASE_NAME} -Fc "${1}"
+        echo "Finished"
     elif [[ "${1}" == *\.sql ]]; then
         echo "psql .sql"
-        PGPASSWORD=${DATABASE_PASSWORD} psql -h ${DATABASE_HOST} -U ${DATABASE_USER} -d ${DATABASE_NAME} -f "${1}"
+        PGPASSWORD=${DATABASE_PASSWORD} psql -h ${DATABASE_HOST} -p ${DATABASE_PORT:-5432} -U ${DATABASE_USER} -d ${DATABASE_NAME} -f "${1}"
+        echo "Finished"
     else
         echo "Unsupported file format"
     fi
