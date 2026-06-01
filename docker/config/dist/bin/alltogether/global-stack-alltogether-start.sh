245ae10c472e5f7b0ea6504b4ff5bc89dfe675e2
Takieddine Messaoudi <takieddine.messaoudi.official@gmail.com>
2026-06-01 23:19:37 +0200

    fix(startup): uncomment error traps in alltogether + android
    The stackCatch + ERR trap blocks were commented out, meaning errors
    during startup went silently undetected. Uncomment the full trap to
    surface failures early.

--- docker/config/dist/bin/alltogether/global-stack-alltogether-start.sh
+++ docker/config/dist/bin/alltogether/global-stack-alltogether-start.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-#set -xeE -o pipefail
-#shopt -s extdebug
-#IFS=$'\n\t'
-#trap 'stackCatch $? $LINENO' EXIT ERR PIPE SIGPIPE SIGHUP
-#stackCatch() {
-#  if [ "${1}" != "0" ]; then
-#    # error handling goes here
-#    echo "Error detected !!"
-#    echo -e "$(date '+%d-%m-%Y %H:%M:%S'): Error - ** line: ${2} ** ** message: ${3} ** global-stack-alltogether-start.sh" >> "${GLOBAL_STACK_DOCKER_TOOLS_PATH}/elapsed"
-#    sleep infinity
-#  fi
-#}
+set -xeE -o pipefail
+shopt -s extdebug
+IFS=$'\n\t'
+trap 'stackCatch $? $LINENO' EXIT ERR PIPE SIGPIPE SIGHUP
+stackCatch() {
+  if [ "${1}" != "0" ]; then
+    # error handling goes here
+    echo "Error detected !!"
+    echo -e "$(date '+%d-%m-%Y %H:%M:%S'): Error - ** line: ${2} ** ** message: ${3} ** global-stack-alltogether-start.sh" >> "${GLOBAL_STACK_DOCKER_TOOLS_PATH}/elapsed"
+    sleep infinity
+  fi
+}
 
 SECONDS=0
 
--- docker/config/dist/bin/android-bin/global-stack-android-start.sh
+++ docker/config/dist/bin/android-bin/global-stack-android-start.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# set -xeE
-# shopt -s extdebug
-# IFS=$'\n\t'
-# trap 'stackCatch ${?} ${LINENO} "${BASH_COMMAND}"' EXIT ERR PIPE SIGPIPE SIGHUP
-# stackCatch() {
-#   if [ "${1}" != "0" ] && [ "${1}" != "1" ]; then
-#     # error handling goes here
-#     echo "Error detected !!"
-#     echo -e "$(date '+%d-%m-%Y %H:%M:%S'): Error - ** line: ${2} ** ** message: ${3} ** global-stack-android-start.sh" >> "${GLOBAL_STACK_DOCKER_TOOLS_PATH}/elapsed"
-#     sleep infinity
-#   fi
-# }
+set -xeE
+shopt -s extdebug
+IFS=$'\n\t'
+trap 'stackCatch ${?} ${LINENO} "${BASH_COMMAND}"' EXIT ERR PIPE SIGPIPE SIGHUP
+stackCatch() {
+  if [ "${1}" != "0" ] && [ "${1}" != "1" ]; then
+    # error handling goes here
+    echo "Error detected !!"
+    echo -e "$(date '+%d-%m-%Y %H:%M:%S'): Error - ** line: ${2} ** ** message: ${3} ** global-stack-android-start.sh" >> "${GLOBAL_STACK_DOCKER_TOOLS_PATH}/elapsed"
+    sleep infinity
+  fi
+}
 
 SECONDS=0
 
